56678d91ea5d1d2addc392e279b21e4a43de31a9
ZhengJie <limzhengjie01@gmail.com>
2024-08-31 21:55:30 +0800

    cleared up css edits

--- Mainpage10East/settings.py
+++ Mainpage10East/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "theme/static",  # Adjust according to your directory structure
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
